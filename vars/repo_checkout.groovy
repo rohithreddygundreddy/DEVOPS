e6bac3cfdd0038db7df8f4ebad23687164100dfe
gundreddyrohithreddy <gundreddyrohithreddy@gmail.com>
2025-11-28 19:14:03 +0530

    maked some changes

--- vars/repo_checkout.groovy
+++ vars/repo_checkout.groovy
@@ -1,16 +1,23 @@
-def call(String repoUrl, string branch ='main', string credentialsId=null){
-    if (credentialsId){
-        checkout([
-            $class: 'GitSCM',
-            branches:[[name:branch]],
-            userRemortConfigs:[[
-                url:repoUrl,
-                credentialsId: credentialsId
-            ]]
-        ])
-    }
-    else{
-        git branch: branch, url:repoUrl
+def call(String repoUrl, String branch = 'main', String credentialsId = null) {
+
+    stage("Validate Repository URL") {
+        if (!repoUrl) {
+            error "Repository URL is required!"
+        }
     }
 
+    stage("Checkout Repository") {
+        if (credentialsId) {
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: "*/${branch}"]],
+                userRemoteConfigs: [[
+                    url: repoUrl,
+                    credentialsId: credentialsId
+                ]]
+            ])
+        } else {
+            git branch: branch, url: repoUrl
+        }
+    }
 }
